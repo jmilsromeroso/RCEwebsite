5de328f8f8558d938c47b634106155f7b6dfe8ef
jazminejuls <jazminejuliagalsim@gmail.com>
2026-04-17 10:21:13 +0800

    contact | map

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -293,7 +293,7 @@ function MapSection() {
     <div style={{ width: '100%', height: '400px', position: 'relative' }}>
       <iframe
         title="GRC Location"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.055961608823!2d120.9858348757451!3d14.652784585841071!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b6002f231e33%3A0xc3f8376e19c01f41!2sGlobal%20Reciprocal%20Colleges!5e0!3m2!1sen!2sph!4v1713214567890!5m2!1sen!2sph"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d965.0193!2d120.9835096!3d14.6498596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b5d4fab883bb%3A0x96f1adb22bed4d5e!2sGlobal%20Reciprocal%20Colleges%20-%20GRC!5e0!3m2!1sen!2sph!4v1744000000000!5m2!1sen!2sph"
         style={{ width: '100%', height: '100%', border: 'none', display: 'block' }}
         loading="lazy" allowFullScreen
       />
